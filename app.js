f20095a56964977144b580a68c01ab4679950bc6
Wilson Serge <sergeshalowilson@gmail.com>
2018-04-30 20:54:49 +0000

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const bodyParser = require("body-parser");
 const express = require("express");
 const ejs = require("ejs");
 const app = express();
@@ -6,7 +5,6 @@ const app = express();
 //app.engine('.ejs', express)
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
@@ -15,11 +13,6 @@ app.get("/", function(req, res) {
 });
 
 
-app.post("/u", function(req, res) {
-    console.log('body: ' + JSON.stringify(req.body));
-    var data = req.body.senderName;
-    res.send(data);
-});
 
 
 app.listen(process.env.PORT, process.env.IP, function() {
